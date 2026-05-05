d829670b7b02496706bf6e1a6a7c839a40b91c2e
Mauryavishal18 <2k23.it2313958@gmail.com>
2026-05-05 14:44:41 +0530

    examples of type alias and literals types

--- L3/app.js
+++ L3/app.js
@@ -1,14 +1,27 @@
 "use strict";
-function combine(num1, num2) {
+// function combine (num1 : number | string, num2:number | string){
+//     let result;
+//     if(typeof num1==="number" && typeof num2=="number"){
+//         result=num1+num2;
+//     }else{
+//         result=num1.toString() + num2.toString();
+//     }
+function combine(num1, num2, conversionType) {
     let result;
-    if (typeof num1 === "number" && typeof num2 == "number") {
-        result = num1 + num2;
+    if (typeof num1 === "number" && typeof num2 == "number" || conversionType === "as-number") {
+        result = +num1 + +num2;
     }
     else {
         result = num1.toString() + num2.toString();
     }
     return result;
 }
-const sum = combine(10, 20);
-const combineName = combine("Patel", "MernStack");
-console.log(sum, combineName);
+const sum1 = combine(10, 20, "as-number");
+const sum2 = combine(10, 50, "as-number");
+const combineName = combine("Patel", "MernStack", "as-string");
+console.log(sum1, sum2, combineName);
+const user = {
+    name: "Maurya",
+    age: 22,
+    skills: ["react", "Node"]
+};
--- L3/app.ts
+++ L3/app.ts
@@ -1,7 +1,45 @@
-function combine (num1 : number | string, num2:number | string){
+// function combine (num1 : number | string, num2:number | string){
+//     let result;
+//     if(typeof num1==="number" && typeof num2=="number"){
+//         result=num1+num2;
+//     }else{
+//         result=num1.toString() + num2.toString();
+//     }
+
+//     return result;
+// }
+
+
+// const sum=combine(10,20);
+// const combineName=combine("Patel","MernStack");
+// console.log(sum,combineName);
+
+//Literals Types
+// function combine (num1 : number | string, num2:number | string, conversionType: "as-number"|"as-string"){
+//     let result;
+//     if(typeof num1==="number" && typeof num2=="number" || conversionType==="as-number"){
+//         result=+num1 + +num2;
+//     }else{
+//         result=num1.toString() + num2.toString();
+//     }
+
+//     return result;
+// }
+
+
+// const sum1=combine(10,20,"as-number");
+// const sum2=combine(10,50,"as-number");
+// const combineName=combine("Patel","MernStack","as-string");
+// console.log(sum1,sum2,combineName);
+
+
+//Type ALIAS /custom types
+type Combinable=number|string;
+type ConversionType="as-number"|"as-string";
+function combine (num1 :Combinable, num2:Combinable, conversionType:ConversionType){
     let result;
-    if(typeof num1==="number" && typeof num2=="number"){
-        result=num1+num2;
+    if(typeof num1==="number" && typeof num2=="number" || conversionType==="as-number"){
+        result=+num1 + +num2;
     }else{
         result=num1.toString() + num2.toString();
     }
@@ -10,6 +48,19 @@ function combine (num1 : number | string, num2:number | string){
 }
 
 
-const sum=combine(10,20);
-const combineName=combine("Patel","MernStack");
-console.log(sum,combineName);
+const sum1=combine(10,20,"as-number");
+const sum2=combine(10,50,"as-number");
+const combineName=combine("Patel","MernStack","as-string");
+console.log(sum1,sum2,combineName);
+
+type User={
+    name:string;
+    age:number;
+    skills:string[]
+}
+
+const user:User={
+    name:"Maurya",
+    age:22,
+    skills:["react","Node"]
+}
